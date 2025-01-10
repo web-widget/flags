8d26638f5dc56929796e86ce45a6e55b1fd51279
Dominik Ferber <dominik.ferber@gmail.com>
2025-01-10 20:35:14 +0200

    use color which does not look like an error message (#17)

--- examples/snippets/components/demo-flag.tsx
+++ examples/snippets/components/demo-flag.tsx
@@ -1,10 +1,10 @@
 export function DemoFlag({ value, name }: { value: boolean; name: string }) {
   return (
     <div
-      className={`rounded-md ${value ? 'bg-green-100 dark:bg-green-800' : 'bg-red-100 dark:bg-red-800'}`}
+      className={`rounded-md ${value ? 'bg-green-100 dark:bg-green-800' : 'bg-fuchsia-100 dark:bg-fuchsia-800'}`}
     >
       <p
-        className={`p-4 text-sm font-medium ${value ? 'text-green-800 dark:text-green-100' : 'text-red-800 dark:text-red-100'}`}
+        className={`p-4 text-sm font-medium ${value ? 'text-green-800 dark:text-green-100' : 'text-fuchsia-800 dark:text-fuchsia-100'}`}
       >
         The feature flag <span className="font-semibold">{name}</span> evaluated
         to <span className="font-semibold">{JSON.stringify(value)}</span>.
