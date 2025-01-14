bb19b9535555fde5a711aaedcb8bada104dd42e8
Dominik Ferber <dominik.ferber@gmail.com>
2025-01-14 18:23:12 +0200

    adapt example (#26)

--- examples/snippets/app/getting-started/overview/page.tsx
+++ examples/snippets/app/getting-started/overview/page.tsx
@@ -1,9 +1,20 @@
+import { flag } from '@vercel/flags/next';
 import { DemoFlag } from '@/components/demo-flag';
-import { randomFlag } from './flags';
 import { ReloadButton } from './reload-button';
 
+// declare a feature flag
+const randomFlag = flag({
+  key: 'random-flag',
+  decide() {
+    // this flag will be on for 50% of visitors
+    return Math.random() > 0.5;
+  },
+});
+
 export default async function Page() {
+  // use the feature flag
   const overview = await randomFlag();
+
   return (
     <>
       <DemoFlag name={randomFlag.key} value={overview} />
