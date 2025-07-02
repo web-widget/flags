4886ebb34dc21950ac1f8041bdcccf78cdc6d2ff
糖饼 <sugarpie.tang@gmail.com>
2025-07-02 11:48:23 +0800

    refactor

--- examples/web-router-example/vite.config.ts
+++ examples/web-router-example/vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { webRouterPlugin } from '@web-widget/vite-plugin';
 import reactWebWidgetPlugin from '@web-widget/react/vite';
-import vuePlugin from '@vitejs/plugin-vue';
-import vueWebWidgetPlugin from '@web-widget/vue/vite';
 import { builtinModules } from 'module';
 
 export default defineConfig({
@@ -21,7 +19,6 @@ export default defineConfig({
       },
     }),
     [react(), reactWebWidgetPlugin()],
-    [vuePlugin(), vueWebWidgetPlugin()],
   ],
   build: {
     target: ['chrome76'],
