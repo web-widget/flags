bbb705d520f01afa3115e4990236998ae5b58b8e
糖饼 <sugarpie.tang@gmail.com>
2025-07-03 10:16:31 +0800

    test: update

--- examples/web-router-example/test/snapshots.test.ts
+++ examples/web-router-example/test/snapshots.test.ts
@@ -10,7 +10,11 @@ describe('Should match snapshot', () => {
   test.each(STATIC_ROUTES.map((route) => [route.pathname]))(
     'Request "%s" should match snapshot',
     async (pathname) => {
-      const result = await fetch(`${pathname}`);
+      const result = await fetch(`${pathname}`, {
+        headers: {
+          cookie: `visitorId=6fcf0060cf63480ab7ae73daee4ad67a`,
+        },
+      });
       expect(result.status).toMatchSnapshot(`${pathname}@status`);
       expect(result.statusText).toMatchSnapshot(`${pathname}@statusText`);
       expect(Object.fromEntries(result.headers.entries())).toMatchSnapshot(
